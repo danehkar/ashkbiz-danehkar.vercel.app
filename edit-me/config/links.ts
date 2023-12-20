4af0bb56f4ba26611a1748be08cbd6766d603985
Ashkbiz Danehkar <ashkbiz.danehkar@gmail.com>
2023-12-20 16:24:44 -0500

    update links.ts

--- edit-me/config/links.ts
+++ edit-me/config/links.ts
@@ -2,34 +2,36 @@ import {
   faGithub,
   faInstagram,
   faLinkedin,
-  faNpm,
+/**  faNpm,*/
   faTwitter,
 } from '@fortawesome/free-brands-svg-icons';
 import { CMSLink } from 'edit-me/types/CMSLink';
 
 export const links: CMSLink[] = [
   {
-    href: 'https://github.com',
+    href: 'https://github.com/danehkar',
     icon: faGithub,
     title: 'GitHub',
   },
   {
-    href: 'https://www.instagram.com',
+    href: 'https://www.instagram.com/ashkbiz.danehkar',
     icon: faInstagram,
     title: 'Instagram',
   },
   {
-    href: 'https://www.linkedin.com',
+    href: 'https://www.linkedin.com/in/danehkar',
     icon: faLinkedin,
     title: 'LinkedIn',
   },
+/**
+*  {
+*    href: 'https://www.npmjs.com',
+*    icon: faNpm,
+*    title: 'NPM',
+*  },
+*/
   {
-    href: 'https://www.npmjs.com',
-    icon: faNpm,
-    title: 'NPM',
-  },
-  {
-    href: 'https://twitter.com',
+    href: 'https://twitter.com/danehkar',
     icon: faTwitter,
     title: 'Twitter',
   },
