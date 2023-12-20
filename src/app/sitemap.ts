8ff5212ab8d2f3d9db0a39fd645433501df5b5b1
Ashkbiz Danehkar <ashkbiz.danehkar@gmail.com>
2023-12-20 18:49:23 -0500

    add sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,8 @@
 import { MetadataRoute } from 'next';
 import { vercelURL } from 'src/helpers/env';
 
-const baseURL = `https://${vercelURL}`;
+/** const baseURL = `https://${vercelURL}`; */
+const baseURL = `https://ashkbiz-danehkar.vercel.app/`;
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
